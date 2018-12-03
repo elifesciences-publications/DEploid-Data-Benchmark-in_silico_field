1a47e5fc3af5c9a0cb6de64593eaeec19c09969a
Joe Zhu <sha.joe.zhu@gmail.com>
2018-12-03 00:02:03 +0000

    include data generation scripts for k_eq_3 3bites

--- fixed_IBD/k_eq_3/3bites.r
+++ fixed_IBD/k_eq_3/3bites.r
@@ -2,7 +2,7 @@
 rm(list=ls());
 source("../../src/src.r")
 
-library(DEploid)
+#library(DEploid)
 args = (commandArgs(TRUE))
 
 # ./3bites.r asiaGroup1 k_eq_3 3bites 1
@@ -12,7 +12,7 @@ k_case = args[2]
 scenario = args[3]
 experimentID = as.numeric(args[4])
 
-panel_prefix = paste(population, k_case, scenario, experimentID, "txt", sep = ".")
+trueHap_prefix = panel_prefix = paste(population, k_case, scenario, experimentID, sep = ".")
 
 #experimentID = 1 #as.numeric(args[1])
 
@@ -27,8 +27,65 @@ err<-0.01;
 fullSampleNames = names(fullPanelFile)[-c(1,2)]
 
 useSamples = sample(fullSampleNames, 20+3, replace = F)
-
+print(useSamples)
 # Use 1-20 samples for panel, 21, 22, 23 for coverage
 
+
+####################################################################
+# Get panel
+####################################################################
 newPanel.col.idx = c(1, 2, 2+which(fullSampleNames %in% useSamples[-c(21:23)]))
 write.table(fullPanelFile[,newPanel.col.idx ], file = paste("panels20/", panel_prefix, ".panel", sep = ""), row.names = F, quote=F, sep="\t")
+
+####################################################################
+# Get True haplotypes
+####################################################################
+totalCoverage = extract_totalCoverage(useSamples[21])
+head(totalCoverage)
+
+red_hap = fullPanelFile[[useSamples[21]]]
+head(red_hap)
+blue_hap = fullPanelFile[[useSamples[22]]]
+green_hap = fullPanelFile[[useSamples[23]]]
+
+new.hap1 = red_hap
+new.hap2 = blue_hap
+new.hap3 = green_hap
+
+new.true = data.frame(CHROM = totalCoverage$CHROM,
+                      POS = totalCoverage$POS,
+                      RED = new.hap1,
+                      GREEN = new.hap2,
+                      BLUE = new.hap3)
+write.table(new.true,
+    file = paste("trueHap/", trueHap_prefix, ".truth", sep=""), row.names = F, quote=F, sep="\t")
+
+####################################################################
+# Get exclude sites at
+####################################################################
+exclude.idx = which(rowSums(fullPanelFile[,2+which(fullSampleNames %in% useSamples)]) == 0)
+write.table(fullPanelFile[exclude.idx, c(1,2)],
+    file = paste("exclude/", trueHap_prefix, ".exclude", sep=""), row.names = F, quote=F, sep="\t")
+
+####################################################################
+# Get coverage with different proportions
+####################################################################
+n.loci = length(red_hap)
+p1_ary = c(10, 10, 10, 10, 10, 10, 15, 15, 15, 15, 20, 20, 20, 25, 30)
+p2_ary = c(10, 15, 20, 25, 30, 40, 15, 20, 25, 30, 20, 25, 30, 25, 30)
+#          80, 75, 70, 65, 60, 50, 70, 65, 60, 55, 60, 55, 50, 50, 40
+for ( pidx in 1:length(p1_ary) ){
+    p1 = p1_ary[pidx]
+    p2 = p2_ary[pidx]
+    p3 = 100 - p1 - p2
+
+    WSAF1 = new.hap1 * p1/100 + new.hap2 * p2/100 + new.hap3 * p3/100
+    includeErrorWSAF1 = WSAF1*(1-err)+(1-WSAF1)*err
+    altCount1 = rbinom(n.loci, totalCoverage$total, includeErrorWSAF1)
+    refCount1 = totalCoverage$total - altCount1
+
+    write.table(data.frame(CHROM = totalCoverage$CHROM, POS = totalCoverage$POS, REF = refCount1),
+        file = paste("alleleCount/",trueHap_prefix, "p", p1,"v", p2, "v", p3, ".ref", sep = ""), row.names = F, quote=F, col.names=T, sep="\t")
+    write.table(data.frame(CHROM = totalCoverage$CHROM, POS = totalCoverage$POS, ALT = altCount1),
+        file = paste("alleleCount/",trueHap_prefix, "p", p1,"v", p2, "v", p3, ".alt", sep = ""), row.names = F, quote=F, col.names=T, sep="\t")
+}
--- src/src.r
+++ src/src.r
@@ -2,5 +2,28 @@ extract_panel <- function(population){
     panelfile = paste("../../field_haps/", population, "Panel_full_final_14_good.csv.gz", sep = "")
     zz=gzfile(panelfile,'rt')
     fullPanelFile = read.csv(zz, header=T, stringsAsFactors = F, check.names = F)
+    close(zz)
     return(fullPanelFile)
+}
+
+
+extract_totalCoverage <- function(sampleName){
+    refFileName = paste("../../readCounts/", sampleName, "_Pf3D7_14_v3_ref.txt.gz", sep="")
+    altFileName = paste("../../readCounts/", sampleName, "_Pf3D7_14_v3_alt.txt.gz", sep="")
+
+    zref = gzfile(refFileName,'rt')
+    ref = read.table(zref, header=T, stringsAsFactors = F, check.names = F)
+
+    zalt = gzfile(altFileName,'rt')
+    alt = read.table(zalt, header=T, stringsAsFactors = F, check.names = F)
+
+    total = ref[,3] + alt[,3]
+    coverage = data.frame(CHROM = ref$CHROM,
+                          POS = ref$POS,
+#                          REF = ref[,3],
+#                          ALT = alt[,3],
+                          total = total)
+    close(zref)
+    close(zalt)
+    return(coverage)
 }
