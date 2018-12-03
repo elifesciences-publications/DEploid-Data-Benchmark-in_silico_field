6be402b57f0f6e04623d2020019e1e9f355127b8
Joe Zhu <sha.joe.zhu@gmail.com>
2018-12-03 15:57:36 +0000

    update run script

--- fixed_IBD/k_eq_3/run.rescomp.deconvolution.k_eq_3.sh
+++ fixed_IBD/k_eq_3/run.rescomp.deconvolution.k_eq_3.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 source ../run.rescomp.deconvolution.src
 
-#experimentCoverage20/experiment100for3mix.dominateIBDwith10v10v80.ref
-
 pvv=( "10v10v80" "10v15v75" "10v20v70" "10v25v65" "10v30v60" "10v40v50" \
  "15v15v70" "15v20v65" "15v25v60" "15v30v55" "20v20v60" "20v25v55" \
  "20v30v50" "25v25v50" "30v30v40" )
 
-pvv=( "10v10v80" "10v15v75" )
-
-
 for vv in "${pvv[@]}"; do
 #echo $vv;
     run_dEploid asiaGroup1 k_eq_3 3bites ${vv}
+    run_dEploid asiaGroup1 k_eq_3 2bites ${vv}
+    run_dEploid asiaGroup1 k_eq_3 1bites ${vv}
 done
