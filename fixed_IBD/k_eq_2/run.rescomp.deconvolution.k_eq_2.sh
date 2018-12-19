9a551eb677c15fdb1c20f9f4af467d24bdabdd66
Joe Zhu <sha.joe.zhu@gmail.com>
2018-12-19 11:59:23 +0000

    update run script for k_2 low and high

--- fixed_IBD/k_eq_2/run.rescomp.deconvolution.k_eq_2.sh
+++ fixed_IBD/k_eq_2/run.rescomp.deconvolution.k_eq_2.sh
@@ -5,8 +5,12 @@ pvv=( "10v90" "20v80"  "30v70" "40v60" "50v50" )
 
 for vv in "${pvv[@]}"; do
 #echo $vv;
-    run_dEploid asiaGroup1 k_eq_2 2bites ${vv}
-    run_dEploid asiaGroup1 k_eq_2 1bites ${vv}
-    run_dEploid africaGroup2 k_eq_2 2bites ${vv}
-    run_dEploid africaGroup2 k_eq_2 1bites ${vv}
+    #run_dEploid asiaGroup1 k_eq_2 2bites ${vv}
+    #run_dEploid asiaGroup1 k_eq_2 1bites ${vv}
+    #run_dEploid africaGroup2 k_eq_2 2bites ${vv}
+    #run_dEploid africaGroup2 k_eq_2 1bites ${vv}
+    run_dEploid asiaGroup1 k_eq_2 1bites_high ${vv}
+    run_dEploid asiaGroup1 k_eq_2 1bites_low ${vv}
+    run_dEploid africaGroup2 k_eq_2 1bites_high ${vv}
+    run_dEploid africaGroup2 k_eq_2 1bites_low ${vv}
 done
